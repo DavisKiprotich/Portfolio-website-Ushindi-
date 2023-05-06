fb4eeaef8da820d24ba11208a750f51b165c39a0
Davis Kiprotich <mylesdavyz@gmail.com>
2023-05-06 20:17:53 +0300

    Created navbar and styled the links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,22 @@ const Navbar = () => {
     return (
       <div classname='header'>
         <Link to='/'>
-        <img src='../Assets/logo.jpeg' alt='logo'/>
+        <img src='../Assets/logo.jpeg' alt='logo' />
         </Link>
+        <ul className='nav-menu'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+          <Link to='/about'>About</Link>
+          </li>
+          <li> 
+            <Link to='/projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
       </div>
     )
   }
