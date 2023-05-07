a1e1deb7acbf82530111237efc9f3a3d91996c53
Davis Kiprotich <mylesdavyz@gmail.com>
2023-05-07 14:22:36 +0300

    Installed react Icons ,styled nav-menu bar with icon and used useState functionality for onlick

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
 import './Navbar.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa'; 
 
 const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click)
+
+
     return (
       <div classname='header'>
         <Link to='/'>
         <img src='../Assets/logo.jpeg' alt='logo' />
         </Link>
-        <ul className='nav-menu'>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li>
             <Link to='/'>Home</Link>
           </li>
@@ -22,6 +28,12 @@ const Navbar = () => {
             <Link to='/contact'>Contact</Link>
           </li>
         </ul>
+        <div className='ham' onClick={handleClick}>
+        {click ? (<FaTimes size={20} style={{ color:'#fff' }} />)
+              : (<FaBars size={20} style={{ color:'#fff' }} />)}
+          
+          
+        </div>
       </div>
     )
   }
