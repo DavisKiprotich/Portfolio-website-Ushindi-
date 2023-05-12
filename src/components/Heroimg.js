fd870e3c11d8d52ab90b2d09139345f9afa5adb9
Davis Kiprotich <mylesdavyz@gmail.com>
2023-05-12 20:16:25 +0300

    Created content section entailing my profession and buttons

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,18 +1,28 @@
-import './HeroStyles.css';
-import React from 'react';
-import BackImg from '../Assets/laptop.jpeg';
+import "./HeroStyles.css";
+import React from "react";
+import BackImg from "../Assets/laptop.jpeg";
+import { Link } from "react-router-dom";
 
 const Heroimg = () => {
   return (
-    <div className='hero'>
-        <div className='mask'>
-            <img className='back-img' src={BackImg} alt='backgroundImg' />
+    <div className="hero">
+      <div className="mask">
+        <img className="back-img" src={BackImg} alt="backgroundImg" />
+      </div>
+      <div className="content">
+        <p>HI, I'M Electrical Engineer</p>
+        <h2>Graphic Designer</h2>
+        <div>
+          <Link to="/projects" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Say Hello
+          </Link>
         </div>
-        <div className='content'>
-          <p></p>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Heroimg
+export default Heroimg;
