754a80d2c3141ea900d3230855ae18f7951f91c7
Davis Kiprotich <mylesdavyz@gmail.com>
2023-05-20 20:52:07 +0300

    Removed footer section

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Heroimg2 from '../components/Heroimg2';
-import Footer from '../components/Footer';
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <Heroimg2 heading='MY PROJECTS.' text='Glimpse of my recent work' />
-      <Footer />
     </div>
   )
 }
