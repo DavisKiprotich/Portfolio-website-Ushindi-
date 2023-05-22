5f69e02039da285e1dc4b39351de67f27f877500
Davis Kiprotich <mylesdavyz@gmail.com>
2023-05-22 22:29:36 +0300

    created contact file

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Heroimg2 from '../components/Heroimg2';
 
 const Contact = () => {
   return (
       <div>
         <Navbar />
+        <Heroimg2 heading='CONTACT' text='Let us have a chat' />
     </div>
   )
 }
