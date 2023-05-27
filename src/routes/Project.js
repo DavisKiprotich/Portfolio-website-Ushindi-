df9891024cd2556fba2a5fe66d6a0379f530910b
Davis Kiprotich <mylesdavyz@gmail.com>
2023-05-27 10:41:18 +0300

    Adding  swiperjs on projects folder

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Heroimg2 from '../components/Heroimg2';
 import PricingCard from '../components/PricingCard';
 
 const Project = () => {
   return (
     <div>
-      <Navbar />
-      <Heroimg2 heading='MY PROJECTS.' text='Glimpse of my recent work' />
+      <Navbar />   
       <PricingCard />
     </div>
   )
