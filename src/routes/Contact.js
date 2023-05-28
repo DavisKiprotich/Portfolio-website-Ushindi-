7217e43d6b270e4b95cc777c23a1d36b88a4877a
Davis Kiprotich <mylesdavyz@gmail.com>
2023-05-28 08:45:41 +0300

    Updated the contact file,styled its components

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Heroimg2 from '../components/Heroimg2';
 import ContactMe from '../components/ContactMe';
 
 const Contact = () => {
   return (
       <div>
         <Navbar />
-        <Heroimg2 heading='CONTACT' text='Let us have a chat' />
         <ContactMe />
     </div>
   )
